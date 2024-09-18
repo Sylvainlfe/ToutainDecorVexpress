2fdda3d77c8aed474e5a97fe457739f5ca6a7a58
sylvain.lafaye <sylvain-lafaye@hotmail.fr>
2024-09-18 16:58:34 +0200

    add react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,42 +1,16 @@
-import { useState, useEffect } from "react";
+import { Outlet } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [message, setMessage] = useState("");
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`${import.meta.env.VITE_API_URL}/api`);
-        if (!response.ok) {
-          throw new Error(`Erreur HTTP! statut: ${response.status}`);
-        }
-        const data = await response.json();
-        console.log(data);
-        setMessage(data.message);
-      } catch (e) {
-        console.error("Une erreur s'est produite:", e);
-        setError("Impossible de se connecter au serveur");
-      }
-    };
-
-    fetchData();
-  }, []);
+  
 
   return (
     <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      {message && <p>Message du serveur : {message}</p>}
-      {error && <p style={{ color: "red" }}>{error}</p>}
+    <Header />
+      <Outlet />
+    <Footer />
     </>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import HomePage from "./pages/HomePage.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
